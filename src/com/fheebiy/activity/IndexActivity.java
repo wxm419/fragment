8fbdbbd23d3aa5bd0f5b63215ecb4e2614999476
zhouwenbo <zhouwenbo550@163.com>
2014-08-04 11:34:48 +0800

    fragment调整

--- src/com/fheebiy/activity/IndexActivity.java
+++ src/com/fheebiy/activity/IndexActivity.java
@@ -3,9 +3,30 @@ package com.fheebiy.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
 import com.fheebiy.R;
+import com.fheebiy.fragment.TabOneFragment;
+import com.fheebiy.fragment.TabTwoFragment;
 
-public class IndexActivity extends FragmentActivity {
+public class IndexActivity extends FragmentActivity implements View.OnClickListener {
+
+    private Button btn1;
+
+    private Button btn2;
+
+    private TabOneFragment oneFragment;
+
+    private TabTwoFragment twoFragment;
+
+    private FragmentManager fragmentManager;
+
+    private FragmentTransaction fragmentTransaction;
+
+
+
     /**
      * Called when the activity is first created.
      */
@@ -13,5 +34,28 @@ public class IndexActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+
+        fragmentManager = getSupportFragmentManager();
+
+        fragmentTransaction = fragmentManager.beginTransaction();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn1:
+
+                break;
+            case R.id.btn2:
+
+                break;
+        }
+
     }
 }
