76558b58b3673dedd1cad2d048fced04ba17f88a
zhouwenbo <zhouwenbo550@163.com>
2014-08-11 18:07:22 +0800

    滑动，listview item，右侧出现删除按钮 进行中

--- src/com/fheebiy/view/MySlideView.java
+++ src/com/fheebiy/view/MySlideView.java
@@ -2,8 +2,11 @@ package com.fheebiy.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Scroller;
 import com.fheebiy.R;
 
 /**
@@ -14,6 +17,16 @@ public class MySlideView extends LinearLayout {
     private Context context;
 
     private LinearLayout content_view;
+
+    private Scroller scroller;
+
+    //试图容器的宽度，120dp
+    private int mHolderWidth = 120;
+
+    //上次滑动时 x坐标
+    private int mLastX = 0;
+    //上次滑动时 y坐标
+    private int mLastY = 0;
 
 
     public MySlideView(Context context) {
@@ -38,11 +51,36 @@ public class MySlideView extends LinearLayout {
     private void initUI() {
         setOrientation(LinearLayout.HORIZONTAL);
         View.inflate(context, R.layout.myslide, this);
-        content_view = (LinearLayout)findViewById(R.id.view_content);
+        content_view = (LinearLayout) findViewById(R.id.view_content);
+
+        scroller = new Scroller(context);
+
+        mHolderWidth = Math.round(TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, mHolderWidth, getResources()
+                .getDisplayMetrics()));
     }
 
-    public void setContentView(View view){
+    public void setContentView(View view) {
         this.content_view.addView(view);
+    }
+
+
+    public void onRequireTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        int scrollX = getScrollX();
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+
+
     }
 
 }
