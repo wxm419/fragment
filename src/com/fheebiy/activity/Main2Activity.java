364c63e61ceab4334b0045a4bb337c0d954bfcf4
zhouwenbo <zhouwenbo550@163.com>
2014-09-24 14:11:37 +0800

    FragmentTabHost 实现

--- src/com/fheebiy/activity/Main2Activity.java
+++ src/com/fheebiy/activity/Main2Activity.java
@@ -3,13 +3,15 @@ package com.fheebiy.activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
-import android.widget.TabHost;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import com.fheebiy.R;
 import com.fheebiy.fragment.TabFiveFragment;
 import com.fheebiy.fragment.TabFourFragment;
 import com.fheebiy.fragment.TabThreeFragment;
 import com.fheebiy.fragment.TabTwoFragment;
-import com.fheebiy.view.TabIndicator;
 import android.widget.TabHost.TabSpec;
 
 /**
@@ -19,50 +21,39 @@ public class Main2Activity extends FragmentActivity {
 
     private FragmentTabHost mTabHost;
 
+    private LayoutInflater inflater;
+
+    private int imags[] = {R.drawable.sy_selector,R.drawable.msg_selector,R.drawable.discovery_selector,R.drawable.my_selector};
+
+    private String text[] = {"搜悦", "消息", "发现", "我的"};
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main2);
-
         mTabHost = (FragmentTabHost) findViewById(R.id.tabhost);
-
         mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        inflater = LayoutInflater.from(this);
+        TabSpec syTabSpec = mTabHost.newTabSpec("sy").setIndicator(getIndicatorView(0));
+        TabSpec msgTabSpec = mTabHost.newTabSpec("msg").setIndicator(getIndicatorView(1));
+        TabSpec disTabSpec = mTabHost.newTabSpec("discovery").setIndicator(getIndicatorView(2));
+        TabSpec myTabSpec = mTabHost.newTabSpec("my").setIndicator(getIndicatorView(3));
 
-        Bundle b = new Bundle();
-        b.putString("key", "sy");
-        TabIndicator syIndicator = new TabIndicator(this);
-        syIndicator.setIconRes(R.drawable.sy_selector);
-        syIndicator.setTitle("搜悦");
-        TabSpec syTabSpec = mTabHost.newTabSpec("sy");
-        syTabSpec.setIndicator(syIndicator);
-
-
-        b = new Bundle();
-        b.putString("key", "msg");
-        TabIndicator msgIndicator = new TabIndicator(this);
-        syIndicator.setIconRes(R.drawable.msg_selector);
-        syIndicator.setTitle("消息");
-        TabSpec msgTabSpec = mTabHost.newTabSpec("msg");
-        msgTabSpec.setIndicator(msgIndicator);
-
-        mTabHost.addTab(syTabSpec, TabTwoFragment.class, b);
-        mTabHost.addTab(msgTabSpec, TabThreeFragment.class, b);
-
-
-      /*  b = new Bundle();
-        b.putString("key", "discovery");
-        TabIndicator disIndicator = new TabIndicator(this);
-        syIndicator.setIconRes(R.drawable.discovery_selector);
-        syIndicator.setTitle("发现");
-        mTabHost.addTab(mTabHost.newTabSpec("discovery").setIndicator(disIndicator), TabFourFragment.class, b);
-
-
-        b = new Bundle();
-        b.putString("key", "my");
-        TabIndicator myIndicator = new TabIndicator(this);
-        syIndicator.setIconRes(R.drawable.my_selector);
-        syIndicator.setTitle("我的");
-        mTabHost.addTab(mTabHost.newTabSpec("my").setIndicator(myIndicator), TabFiveFragment.class, b);*/
-
+        mTabHost.addTab(syTabSpec, TabTwoFragment.class, null);
+        mTabHost.addTab(msgTabSpec, TabThreeFragment.class, null);
+        mTabHost.addTab(disTabSpec, TabFourFragment.class, null);
+        mTabHost.addTab(myTabSpec, TabFiveFragment.class, null);
 
     }
+
+
+    private View getIndicatorView(int i){
+        View view = inflater.inflate(R.layout.tab_indicator,null);
+        ImageView img = (ImageView)view.findViewById(R.id.indicator_img);
+        TextView tv = (TextView)view.findViewById(R.id.indicator_text);
+        img.setImageResource(imags[i]);
+        tv.setText(text[i]);
+
+        return view;
+    }
+
 }
